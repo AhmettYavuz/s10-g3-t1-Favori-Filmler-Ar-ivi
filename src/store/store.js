ab54d44b6d5edc24020612ecd322c39616d6eed4
AhmettYAVUZ <yavuzahmet555@gmail.com>
2024-05-02 16:16:43 +0300

    son güncelleme

--- src/store/store.js
+++ src/store/store.js
@@ -1,7 +1,11 @@
 import { legacy_createStore as createStore, applyMiddleware } from "redux";
 import reducer from "./reducers";
 import logger from "redux-logger";
+import { composeWithDevTools } from "@redux-devtools/extension";
 
-const myStore = createStore(reducer, applyMiddleware(logger));
+const myStore = createStore(
+  reducer,
+  composeWithDevTools(applyMiddleware(logger))
+);
 
 export default myStore;
